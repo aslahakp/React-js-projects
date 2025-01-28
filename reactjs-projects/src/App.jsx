afe3d6aae3377cdb9eb588f365a3212d0d0366ed
aslahakp <aslahakp786@gmail.com>
2025-01-28 18:39:49 +0300

    first commit

---
+++ reactjs-projects/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Accordian from './components/accordian'
+
+const App = () => {
+  return (
+    <div>
+      {/* Accordian */}
+      <Accordian />
+    </div>
+  )
+}
+
+export default App
---
+++ reactjs-projects/src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: '1',
+        question : "can i cretae accordian component with another framework?",
+        answer:"yes, it is possible"
+    },
+    {
+        id: '2',
+        question : "can i cretae accordian component with another framework?",
+        answer:"yes, it is possible"
+    },
+    {
+        id: '3',
+        question : "can i cretae accordian component with another framework?",
+        answer:"yes, it is possible"
+    },
+    {
+        id: '4',
+        question : "can i cretae accordian component with another framework?",
+        answer:"yes, it is possible"
+    }
+];
+export default data;
